3a145e4c9c37dcad54871b9edcc8eba09150decc
s-oa18 <sethampofo.sa@gmail.com>
2023-02-16 18:45:44 -0800

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * Return: 0 when successful
 */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,");
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
